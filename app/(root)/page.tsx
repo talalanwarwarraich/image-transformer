f0db7ac2099f83d4bd0637d6cde592b7d08d2ca1
Talal Anwar <talal.anwar.warraich@gmail.com>
2024-02-17 12:51:10 +0500

    clerk authentication implemented.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,12 @@
+import { UserButton } from '@clerk/nextjs';
 import React from 'react';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className='h-screen'>
+      <UserButton afterSignOutUrl='/' />
+    </div>
+  );
 };
 
 export default Home;
